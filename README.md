a090347f14e968ea3ee955a321d1fd753f996e80
Matthieu Dorier <mdorier@anl.gov>
2020-12-01 18:36:52 +0000

    started client

--- README.md
+++ README.md
@@ -3,3 +3,33 @@ Py-Bedrock
 
 Py-Bedrock providers Python utilities to configure and deploy Mochi-based
 services using Python.
+
+Running the Jupyter demo
+------------------------
+
+Create a spack environment and add the required packages in it.
+
+```
+spack env create py-bedrock-demo
+spack env activate py-bedrock-demo
+spack add py-mochi-bedrock
+spack add py-jupyterlab-server
+spack install
+```
+
+Deactivate and re-activate the environment for the PYTHONPATH variable to
+be updated.
+
+```
+spack env deactivate
+spack env activate py-bedrock-demo
+```
+
+Run the Jupyter server.
+
+```
+jupyter notebook --ip 0.0.0.0 --port 8888
+```
+
+Then from your browser, open the `notebooks/Demo.ipynb` notebook,
+and start playing!
---
+++ bedrock/client.py
@@ -0,0 +1,37 @@
+# (C) 2018 The University of Chicago
+# See COPYRIGHT in top-level directory.
+
+
+"""
+.. module:: client
+   :synopsis: This package provides access to the Bedrock C++ wrapper
+
+.. moduleauthor:: Matthieu Dorier <mdorier@anl.gov>
+
+
+"""
+
+
+import _pybedrock
+import pymargo.core
+import pymargo
+
+
+class Client(_pybedrock.Client):
+
+    def __init__(self, arg):
+        if isinstance(arg, pymargo.core.Engine):
+            super().__init__(arg.get_internal_mid())
+            self._engine = None
+        elif isinstance(arg, str):
+            self._engine = pymargo.core.Engine(
+                arg, pymargo.client)
+            super().__init__(self._engine.get_internal_mid())
+        else:
+            raise TypeError(f'Invalid argument type {type(arg)}')
+
+    def __del__(self):
+        super().__del__()
+        if self._engine is not None:
+            self._engine.finalize()
+            del self._engine
